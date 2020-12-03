20c8adfa2b625b7fae69d231fcfce8765998c864
ericvbueno <ericv.bueno@gmail.com>
2020-12-03 14:40:22 -0300

    nivel usuarios

--- Projeto_CRUD/usuarios.php
+++ Projeto_CRUD/usuarios.php
@@ -1,18 +1,7 @@
 <?php
 session_start();
 include_once("conexao.php");
-// A sessão precisa ser iniciada em cada página diferente
-if (!isset($_SESSION)) session_start();
-
-$nivel_necessario = 1;
-
-// Verifica se não há a variável da sessão que identifica o usuário
-if (!isset($_SESSION['UsuarioID']) or ($_SESSION['UsuarioNivel'] < $nivel_necessario)) {
-
-  // Redireciona o usuario para sua lista de clientes
-  header("Location: login.php");
-  exit();
-}
+include_once("testar_nivel.php");
 
 ?>
 <!DOCTYPE html>
